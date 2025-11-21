6546a131380d24b32cea2bce5986ccac0f214354
Diego-132hub <garciaramirezdiegoandres7@gmail.com>
2025-11-21 16:53:57 -0500

    config: update base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gametracker-frontend/', // ¡IMPORTANTE para GitHub Pages!
+  base: '/gametracker-frontend/', // Para GitHub Pages de Diego-132hub
   build: {
     outDir: 'dist',
     sourcemap: true
